a72c5459ad819c7a2bd9b418b445f7a9868e8d25
KDH20160140 <bzidol@naver.com>
2021-02-18 16:55:31 +0900

    21-02-18 예약 상세페이지 구현중 (관리자가 아닌 사용자 기준 예약 페이지)
    - 기본 틀 제작
    - 입력값 예외처리중

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,24 @@
   <v-app>
     <pcMenuBar />
     <v-main>
-      <HelloWorld />
+      <showDetail />
+      <!-- <HelloWorld /> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+// import HelloWorld from "./components/HelloWorld";
 import pcMenuBar from "@/components/pcMenuBar";
+import showDetail from "@/views/showDetail";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
     pcMenuBar,
+    showDetail,
   },
 
   data: () => ({
